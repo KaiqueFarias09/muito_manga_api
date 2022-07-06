1889152ac712b1f079ec2444b2da29224df3656d
Kaique <kaiquesouzafarias@outlook.com>
2022-07-06 00:59:18 -0300

    Fiz a criação da função que faz o scrapping da lista de mangás

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,16 +1,36 @@
 import axios from 'axios';
 import { load } from 'cheerio';
 
-const MAIN_URL = 'https://muitomanga.com/';
+const MAIN_URL = 'https://muitomanga.com';
 
 export async function scrapeMangaList({ keyw = '', orby = '', inGenre = '' }) {
     try {
-        // let data = [];
+        let data = [];
 
-        const page = await axios.get(`${MAIN_URL}lista-de-mangas${orby}`);
+        const page = await axios.get(`${MAIN_URL}/lista-de-mangas${orby}`);
         const scrappedPage = load(page.data);
 
-        console.log(scrappedPage);
+        scrappedPage('div.postagem_manga').each((i, el) => {
+            const manga = scrappedPage(el).find('div:nth-child(1)');
+
+            data.push({
+                index: i,
+                title: manga
+                    .find(
+                        'a:nth-child(1) > div:nth-child(2) > div:nth-child(1)'
+                    )
+                    .text()
+                    .trim(),
+                img: manga
+                    .find(
+                        'a:nth-child(1) > div:nth-child(1) > img:nth-child(2)'
+                    )
+                    .attr('data-src'),
+                src: `${MAIN_URL}${manga.find('a:nth-child(1)').attr('href')}`,
+            });
+        });
+        console.log(data);
+        return data;
     } catch (err) {
         console.error(err);
     }
